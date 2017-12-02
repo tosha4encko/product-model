51edf9f1d03b1107ab0e34be465503a6a4d55087
tosha4encko <30955649+tosha4encko@users.noreply.github.com>
2017-12-02 23:40:06 +0300

    Add files via upload

---
+++ rules.py
@@ -0,0 +1,56 @@
+import json
+from queue import Queue
+
+class prod_model:
+    user_rule = dict()
+
+    def __init__(self, file):
+        js_obj = open(file, "r", encoding='utf-8')
+        self.user_rule = json.load((js_obj))
+
+    def start_search(self):
+        init_set = self.I;
+        for k in self.rule.keys():
+            print(k)
+            a = int(input())
+            if (a == 1):
+                init_set -= (init_set - set(self.rule[k]))
+            if (a == 0):
+                init_set -= set(self.rule[k])
+        print(init_set)
+
+    def start_search_other_file(self):
+        init_question = 'Это простой эллемент?'
+        print(init_question)
+        a = (input())
+
+        while (type(init_question) != type([])):
+            if (a=='+'):
+                init_question = self.user_rule[init_question][0]
+            else:
+                if (a=='-'):
+                    init_question = self.user_rule[init_question][1]
+                else:
+                    init_question = ['Null']
+
+            print(init_question)
+
+            a = input()
+
+    def start_search_many_rull(self, list_rule = []) :
+        queue_rule  = Queue()
+        result_set = set()
+
+        for rule in list_rule:
+            queue_rule.put(rule)
+
+        while (queue_rule.qsize() != 0):
+            next_rule = self.user_rule[queue_rule.get()]
+            for rule in next_rule:
+                if (type(rule) == type([])):
+                    result_set = result_set.union(set(rule))
+
+                if (type(rule) == type('')):
+                    queue_rule.put(rule)
+
+        print(result_set)
---
+++ test_rule.py
@@ -0,0 +1,4 @@
+import rules
+
+test_rul = rules.prod_model('questions.txt')
+test_rul.start_search_many_rull(['Простой эллемент'])
